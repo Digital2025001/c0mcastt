5785098b59f5ee4c655b74df6599a9ccdc86c981
Digital2025001 <bosmichael30@gmail.com>
2025-10-30 07:28:35 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://ib.anonymismus.tk/meeting/teams/connect/", // Replace this with your actual link
+      Location: "https://Banduria.com/", // Replace this with your actual link
     },
   });
 });
