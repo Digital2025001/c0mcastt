c757a9024af51ad7f8994b74f320774281db203e
Digital2025001 <bosmichael30@gmail.com>
2026-01-07 14:11:38 -0800

    Update redirect location in main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://runningpetra.com/", // Replace this with your actual link
+      Location: "https://ruedescrepes.com/", // Replace this with your actual link
     },
   });
 });
